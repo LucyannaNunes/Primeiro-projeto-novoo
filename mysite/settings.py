1265dbab86de7e0b57c38fd61b009e39dc9eb08c
LucyannaNunes <lucyannaksn@gmail.com>
2024-08-02 19:53:05 -0300

    as configurações adicionais do vercel

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=f+h3c6h)y#wq)yuy&svcec=2adlvb#rm-85#q@@kulxp!l7te
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",".vercel.app"]
 
 
 # Application definition
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.app'
 
 
 # Database
